099c67377eb64f41851e4322ce212539c6c06e49
Ítalo Reis Rosa da Silva <italoreisrosa@gmail.com>
2023-09-11 10:40:35 -0300

    logica dos components de "read more" feita
    preciso polir o design deles e adicionar os texto corretos para cada componente.

--- pages/userScreen/ajuda/ajudaScreen.tsx
+++ pages/userScreen/ajuda/ajudaScreen.tsx
@@ -4,23 +4,58 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { ImageBackground } from 'react-native';
+import ReadMoreText from './textExpand';
 const Stack = createNativeStackNavigator();
 
 interface AjudaScreenProps {
   navigation: any; // 
 }
-export default function AjudaScreen({ navigation }: AjudaScreenProps)  {
-  
+export default function AjudaScreen({ navigation }: AjudaScreenProps) {
+
   return (
 
+    <ScrollView>
     <View style={styles.container}>
-      <View style={styles.header}>
-                <FontAwesome5 name="bars" size={40} color="black" />
-                <Text style={styles.title}>Ajuda</Text>
-                <Image source={require('../ajuda/imgs/profilePicUser.png')} style={{ height: 70, width: 70 }} />
-            </View>
+    <View style={styles.header}>
+      <FontAwesome5 name="bars" size={40} color="black" />
+      <Text style={styles.title}>Ajuda</Text>
+      <Image source={require('../ajuda/imgs/profilePicUser.png')} style={{ height: 70, width: 70 }} />
     </View>
-   
+    <View>
+      <Image source={require('./imgs/imgAjuda.png')}></Image>
+    </View>
+    <ReadMoreText
+      initialText={`Tiver idade inferior a 16 anos ou superior a 69 anos.
+        Obs.: o limite superior para a primeira doação é 60 anos. Quem tem 61 anos ou mais e nunca doou está inapto.
+        Tiver peso inferior a 50 quilos.
+        Estiver com anemia no teste realizado imediatamente antes da doação.
+        Estiver com hipertensão ou hipotensão arterial no momento da doação.
+        Estiver com aumento ou diminuição dos batimentos cardíacos no momento da doação.
+        Estiver com febre no dia da doação.
+        Estiver grávida.
+        Estiver amamentando, a menos que o parto tenha ocorrido há mais de 12 meses.
+        Obs.: o doador não poderá doar se vier acompanhado de crianças menores de 13 anos sem a presença de um outro adulto para cuidar delas.`}
+      maxLength={200}
+      titleWhenClosed="Clique para ler mais"
+      titleWhenOpen="Clique para fechar"
+    />
+     <ReadMoreText
+      initialText={`Tiver idade inferior a 16 anos ou superior a 69 anos.
+        Obs.: o limite superior para a primeira doação é 60 anos. Quem tem 61 anos ou mais e nunca doou está inapto.
+        Tiver peso inferior a 50 quilos.
+        Estiver com anemia no teste realizado imediatamente antes da doação.
+        Estiver com hipertensão ou hipotensão arterial no momento da doação.
+        Estiver com aumento ou diminuição dos batimentos cardíacos no momento da doação.
+        Estiver com febre no dia da doação.
+        Estiver grávida.
+        Estiver amamentando, a menos que o parto tenha ocorrido há mais de 12 meses.
+        Obs.: o doador não poderá doar se vier acompanhado de crianças menores de 13 anos sem a presença de um outro adulto para cuidar delas.`}
+      maxLength={200}
+      titleWhenClosed="Clique para ler mais"
+      titleWhenOpen="Clique para fechar"
+    />
+  </View>
+  </ScrollView>
   );
 }
 
@@ -42,14 +77,14 @@ const styles = StyleSheet.create({
     width: '100%',
     display: 'flex',
     flexDirection: 'row',
-    alignItems:`center`,
+    alignItems: `center`,
     gap: 90,
     paddingLeft: 30,
     paddingTop: 20,
-},
-title: {
+  },
+  title: {
     fontSize: 30,
     fontWeight: '300',
     color: 'black',
-}
+  },
 });
---
+++ pages/userScreen/ajuda/textExpand.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+interface ReadMoreTextProps {
+  initialText: string;
+  maxLength: number;
+  titleWhenClosed: string;
+  titleWhenOpen: string;
+}
+
+const ReadMoreText: React.FC<ReadMoreTextProps> = ({
+  initialText,
+  maxLength,
+  titleWhenClosed,
+  titleWhenOpen,
+}) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
+
+  const displayText = expanded ? initialText : initialText.slice(0, maxLength);
+
+  return (
+    <View style={styles.component}>
+      <TouchableOpacity onPress={toggleExpand} style={styles.header}>
+        <Text style={styles.titleWhenClosed}>
+          {expanded ? titleWhenOpen : titleWhenClosed}
+        </Text>
+        <FontAwesome5
+          name={expanded ? 'arrow-up' : 'arrow-down'}
+          size={18}
+        />
+      </TouchableOpacity>
+
+      {expanded && (
+        <Text>{displayText}</Text>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  component: {
+    flexDirection: 'column',
+    width: '80%',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  titleWhenClosed: {
+    fontSize: 24,
+    fontWeight: '200',
+    marginRight: 10,
+  },
+});
+
+export default ReadMoreText;
