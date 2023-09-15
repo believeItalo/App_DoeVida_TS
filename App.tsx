461b7f1d52221383630ca8db4185d512ab76e237
Ítalo Reis Rosa da Silva <italoreisrosa@gmail.com>
2023-09-15 09:09:52 -0300

    navigator e drawer funcionando juntos
    porem o drawer ta aparecendo em todas as telas, preciso retirar e deixar ele visivel em apenas algumas telas

--- App.tsx
+++ App.tsx
@@ -6,8 +6,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import 'react-native-gesture-handler';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
-//import de componenetes
-import CustomDrawerHeader from './drawerEstilo';
 //IMPORT DAS TELAS
 import HomeScreen from './pages/homeScreen/homeScreen';
 import LoginScreen from './pages/loginScreen/loginScreen';
@@ -30,30 +28,12 @@ const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
 
 
-//ESTILO DRAWER
-
-//DRAWER NAVIGATION
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="BuscaHemocentro">
-        <Drawer.Screen name="BuscaHemocentro" component={BuscaHemocentroScreen} options={{}}></Drawer.Screen>
-        <Drawer.Screen name="MeuPerfil" component={MeuPerfilScreen} />
-        <Drawer.Screen name="EditarPerfil" component={EditarPerfilScreen}/>
-        <Drawer.Screen name="RedefinirSenha" component={RedefinirSenhaScreen} />
-        <Drawer.Screen name="AgendaDisponivelHemocentro" component={AgendaDisponivelHemocentroScreen} />
-        <Drawer.Screen name="QuemPodeDoar" component={QuemPodeDoarScreen} />     
-      </Drawer.Navigator>
-    </NavigationContainer>
-    
-  );
-}
-
 //NAVIGATOR STACK
-function StackScreen() {
+
+function HomeStack() {
   return (
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+    <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
       <Stack.Screen name="CadastroInformacoesPessoais" component={CadastroScreen} options={{ headerShown: false }} />
       <Stack.Screen name="CadastroTipoSanguineo" component={CadastroTipoSanguineoScreen} options={{ headerShown: false }} />
@@ -69,3 +49,23 @@ function StackScreen() {
     </Stack.Navigator>
   );
 }
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeStack} />
+        <Drawer.Screen name="BuscaHemocentro" component={BuscaHemocentroScreen}/>
+        <Drawer.Screen name="MeuPerfil" component={MeuPerfilScreen} />
+        <Drawer.Screen name="EditarPerfil" component={EditarPerfilScreen} />
+        <Drawer.Screen name="RedefinirSenha" component={RedefinirSenhaScreen} />
+        <Drawer.Screen name="AgendaDisponivelHemocentro" component={AgendaDisponivelHemocentroScreen} />
+        <Drawer.Screen name="QuemPodeDoar" component={QuemPodeDoarScreen} />
+        <Drawer.Screen name="Ajuda" component={AjudaScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
